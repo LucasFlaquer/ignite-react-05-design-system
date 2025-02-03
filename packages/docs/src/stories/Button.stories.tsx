b990c06ec80d75c672cdb255128d58aa05cc006b
lucasflaquer <lucas.flaquer@gmail.com>
2025-02-03 16:44:54 -0300

    button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+import { ArrowRight } from '@phosphor-icons/react'
+
+const meta = {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export default meta
+
+type Story = StoryObj<ButtonProps>
+
+export const Primary: Story = {}
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Cancel: Story = {
+  args: {
+    variant: 'tertiary',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: Story = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+}
