0cbcb3484f8614f622ab62ef5ccb1c5e55b4caed
lucasflaquer <lucas.flaquer@gmail.com>
2025-02-07 13:01:43 -0300

    multistep component

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,37 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+const meta = {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+        >
+          {story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export default meta
+
+type Story = StoryObj<MultiStepProps>
+
+export const Primary: Story = {
+  args: {},
+}
+
+export const Full: Story = {
+  args: {
+    currentStep: 4,
+  },
+}
